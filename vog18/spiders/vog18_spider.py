2d53510fb75076a673120dc7424c378deb690591
Bishop-dev <alex.bishop@yandex.ru>
2014-07-30 23:38:40 +0300

    Extracting of photo links

--- vog18/spiders/vog18_spider.py
+++ vog18/spiders/vog18_spider.py
@@ -1,11 +1,20 @@
 from scrapy.contrib.spiders.crawl import CrawlSpider
+from scrapy.selector import Selector
 
 
 class VOG18_Spider(CrawlSpider):
+    filename = 'page.html'
     name = 'VOG18_spider'
     allowed_domains = ['vk.com', 'pp.vk.me']
     start_urls = ['https://m.vk.com/vog18']
 
     def parse(self, response):
-        with open('filename.html', 'wb') as f:
-            f.write(response.body)
+        xpath = "/html/body/div[3]/div[2]/div[2]/div/div[2]/div[2]/div[{0}]/div[2]/div[1]/div/a/img"
+        hxs = Selector(response)
+        self.save_page(response.body)
+        for x in range(1, 6):
+            print(hxs.xpath(xpath.format(x) + '/@data-src_big').extract())
+
+    def save_page(self, content):
+        with open(self.filename, 'wb') as f:
+            f.write(content)
