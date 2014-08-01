c7206842b3cefdb72ea87915a852360647c8c465
Bishop-dev <alex.bishop@yandex.ru>
2014-08-01 23:47:54 +0300

    Ability to save photo name and link to mongoDB

--- vog18/spiders/vog18_spider.py
+++ vog18/spiders/vog18_spider.py
@@ -3,10 +3,14 @@ import uuid
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders.crawl import CrawlSpider, Rule
 from scrapy.selector import Selector
+from vog18.items import Vog18Item
 
 
 class VOG18_Spider(CrawlSpider):
-    filename = 'page{0}.html'
+    page_name = 'page{0}.html'
+    photo_name = 'photo{0}.jpg'
+    xpath = '/html/body/div[3]/div[2]/div[2]/div/div[2]/div[3]/div[{0}]/div[2]/div[1]/div/a/img'
+    attribute = '/@data-src_big'
     name = 'VOG18_spider'
     allowed_domains = ['vk.com']
     start_urls = ['https://m.vk.com/vog18']
@@ -19,16 +23,21 @@ class VOG18_Spider(CrawlSpider):
     ]
 
     def parse_public(self, response):
-        xpath = '/html/body/div[3]/div[2]/div[2]/div/div[2]/div[3]/div[{0}]/div[2]/div[1]/div/a/img'
-        attribute = '/@data-src_big'
         self.save_page(response.body)
         hxs = Selector(response)
+        items = []
         for index in range(1, 11):
-            elements = hxs.xpath(xpath.format(index) + attribute).extract()
+            elements = hxs.xpath(self.xpath.format(index) + self.attribute).extract()
             if len(elements) > 0:
                 url = elements[0].split('|')[0]
-                urllib.urlretrieve(url, 'photo{0}.jpg'.format(uuid.uuid4()))
+                file_photo_name = self.photo_name.format(uuid.uuid4())
+                urllib.urlretrieve(url, file_photo_name)
+                item = Vog18Item()
+                item['link'] = url
+                item['photo_name'] = file_photo_name
+                items.append(item)
+        return items
 
     def save_page(self, content):
-        with open(self.filename.format(uuid.uuid4()), 'wb') as f:
+        with open(self.page_name.format(uuid.uuid4()), 'wb') as f:
             f.write(content)
